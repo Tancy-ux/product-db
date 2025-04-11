9c182cce500ecb6e3d004210edf0fdb02ebf2bca
Tancy-ux <tanushree@wareinnovations.com>
2025-04-11 11:25:37 +0530

    fix minor

--- src/bulk.js
+++ src/bulk.js
@@ -1,6 +1,6 @@
 // import dotenv from "dotenv";
 // import mongoose from "mongoose";
-// import BaseColor from "./models/BaseColor.js";
+// import GeneralColor from "./models/GeneralColor.js";
 
 // dotenv.config();
 
@@ -12,37 +12,18 @@
 //         console.error("Error connecting to MongoDB:", error.message);
 //     }
 // }
-
-// const baseColors = [
-//   { name: 'Gloss Misty Rose' },
-//   { name: 'Matte Misty Rose' },
-//   { name: 'Matte Jet Black' },
-//   { name: 'Gloss Stormy Blue' },
-//   { name: 'Gloss Tea Green' },
-//   { name: 'Matte Nude' },
-//   { name: 'Gloss Sage Green' },
-//   { name: 'Gloss Warm Brown' },
-//   { name: 'Matte Tan' },
-//   { name: 'Gloss Nude' },
-//   { name: 'Unglazed' },
-//   { name: 'Gloss Aqua' },
-//   { name: 'Matt Black' },
-//   { name: 'Gloss Pink' },
-//   { name: 'Steel Blue' },
-//   { name: 'Steel Blue with a Band' },
-//   { name: 'Gloss Black' },
-//   { name: 'Gloss Cherry Red' },
-//   { name: 'Deep Blue' }
-// ];
-
 // async function seedBaseColors() {
 //   try {
 //     await connectDB();
-//     // Optional: Clear existing data
-//     await BaseColor.deleteMany({});
+
+//     const result = await GeneralColor.updateMany(
+//         { material: { $regex: /^Mats$/i } },
+//         { $set: { material: "Cork" } }
+//       );
+  
+//       console.log(`Updated ${result.modifiedCount} documents from pink to blue`);
+      
     
-//     // Insert new data
-//     await BaseColor.insertMany(baseColors);
     
 //     console.log('Base colors seeded successfully');
 //   } catch (error) {
