c31d8199dc19d9787a1a0967d1d07b9958f6e591
Tancy-ux <tanushree@wareinnovations.com>
2025-07-08 16:49:21 +0530

    cors def added

--- src/server.js
+++ src/server.js
@@ -10,7 +10,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://sku-generator-mu.vercel.app'
+}));
 app.use(express.json());
 
 app.use("/api/sku", skuRoutes);
